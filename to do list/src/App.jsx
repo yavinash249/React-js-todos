88520b7a18f331a5d4299a92c729530ea54049e1
Avinash Yadav <95971529+yavinash249@users.noreply.github.com>
2023-06-20 10:50:09 +0530

    react todos form
    this is

---
+++ to do list/src/App.jsx
@@ -0,0 +1,50 @@
+import { useEffect, useState } from "react";
+import "./style.css";
+import { NewTodofrom } from "./NewTodofrom";
+import { TodoList } from "./TodoList";
+
+export default function App(){
+  
+   const [todos, setTodos] = useState(()=> {
+    const localValue = localStorage.getItem("ITEMS")
+    if (localValue == null) return []
+    return JSON.parse(localValue)
+   }) 
+
+  useEffect(()=>{
+    localStorage.setItem("ITEMS", JSON.stringify(todos));
+  },[todos] )
+
+   function addTodo(title){
+    setTodos(currenTodos=> {
+      return[ 
+        ...currenTodos,
+        {id: crypto.randomUUID() , title, completed: false},
+       ]
+    })
+   }
+  
+  function toggleTodo(id, completed) {
+    setTodos(currentTodos =>{
+      return currentTodos.map(todo=> {
+        if(todo.id === id){
+          return {...todo, completed}
+        }
+        return todo
+      })
+    })
+  }
+  function deleteTodo(id){
+    setTodos(currentTodos => {
+      return currentTodos.filter(todo=> todo.id !== id)
+    })
+  }
+
+  return  (
+    <>
+      <NewTodofrom onSubmit={addTodo} />
+       <h1 className="header">To Do list</h1>
+       <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo}/>
+    </>
+  )
+}
